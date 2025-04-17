ca14d8810d3581cca6f9df66e4db27064e90175a
Deni0012 <deni.dg00@icloud.com>
2025-04-17 10:59:37 +0200

    feat: first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route >
+            <Route />
+            <Route />
+            <Route />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App;
